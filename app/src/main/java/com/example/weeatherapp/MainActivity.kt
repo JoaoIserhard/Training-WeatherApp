ab397bfd278a0d772e8d4da5af27b530b181f669
JoaoIserhard <jiserhard@gmail.com>
2026-03-22 09:17:08 -0300

    [feat]Add Navigation + Splash Screen

--- app/src/main/java/com/example/weeatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weeatherapp/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.weeatherapp.navigation.WeatherNavigation
 import com.example.weeatherapp.ui.theme.WeeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,16 +24,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WeeatherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            WeatherApp()
+        }
+    }
+}
+
+@Composable
+fun WeatherApp() {
+    WeeatherAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(innerPadding).fillMaxSize()
+            ) {
+                WeatherNavigation()
             }
         }
     }
+
 }
 
 @Composable
---
+++ app/src/main/java/com/example/weeatherapp/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.example.weeatherapp.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
